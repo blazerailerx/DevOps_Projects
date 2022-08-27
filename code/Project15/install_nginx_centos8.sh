d3ec880d703841c8888c96f061438c1262e91f0a
Ihechi Okere <ihechi@live.com>
2022-08-27 09:11:53 +0000

    ongoing

---
+++ code/Project15/install_nginx_centos8.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /etc/yum.repos.d/
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+sudo yum update -y
+sudo yum install -y nginx git
+sudo systemctl restart nginx
